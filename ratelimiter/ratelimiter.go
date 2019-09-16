6c0c72fe5dc61f7e971bcca151ca0a85552f0f2f
Deepak Kumar <deepak@plivo.com>
2019-09-16 16:40:31 +0530

    basic limiter

--- ratelimiter/ratelimiter.go
+++ ratelimiter/ratelimiter.go
@@ -1,8 +1,23 @@
+package ratelimiter
+
+import (
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
+    "net/http"
+)
+
+type Response struct {
+    Success bool
+    Message string
+    Body    interface{}
+    APIID   string `json:"apiID"`
+}
+
 func RateLimitMiddleware() echo.MiddlewareFunc {
     return func(next echo.HandlerFunc) echo.HandlerFunc {
         return func(c echo.Context) error {
             if hasLimitExceeded() {
-                return c.JSON(http.StatusTooManyRequests, structs.Response{Success: false, Message: "OK"})
+                return c.JSON(http.StatusTooManyRequests, Response{Success: false, Message: "OK"})
             }
             return next(c)
         }
